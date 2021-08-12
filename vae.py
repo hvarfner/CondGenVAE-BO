fd5d51630ff1e7ba0005754c71029fdfcdad1686
Matthias Mayr <matthias.mayr@cs.lth.se>
2021-08-12 11:52:26 +0200

    Chore: Applies pycodestyle.

--- vae.py
+++ vae.py
@@ -10,7 +10,7 @@ from jax import jit, grad, lax, random
 from jax.experimental import optimizers
 from jax.experimental import stax
 from jax.experimental.stax import Dense, FanOut, Relu, Softplus,\
-     Sigmoid, Conv, BatchNorm, Flatten, ConvTranspose, Softmax, Dropout
+    Sigmoid, Conv, BatchNorm, Flatten, ConvTranspose, Softmax, Dropout
 from jax.random import multivariate_normal
 import numpy as np
 import tensorflow_probability as tfp
@@ -23,11 +23,13 @@ LATENT_SIZE = 2
 IMAGE_SHAPE = (28, 28)
 FASHION = True
 
+
 def Reshape(new_shape):
     """Layer construction function for flattening all but the leading dim."""
     def init_fun(rng, input_shape):
         output_shape = (input_shape[0],) + new_shape
         return output_shape, ()
+
     def apply_fun(params, inputs, **kwargs):
         output_shape = (inputs.shape[0],) + new_shape
         return jnp.reshape(inputs, output_shape)
@@ -53,7 +55,7 @@ def sample_latent_space(rng, params, images):
     mu_z, sigmasq_z = encode(encoder_params, images)
     sample = gaussian_sample(rng, mu_z, sigmasq_z)
     return sample
-    
+
 
 def elbo(rng, params, images, beta=1):
     encoder_params, decoder_params, _ = params
@@ -82,10 +84,10 @@ def elbo_and_pred_loss(rng, params, images, labels, beta, pred_weight, n_samples
         logits = decode(decoder_params, samples)
         iwelbo_loss += bernoulli_logpdf(logits, images) - beta * gaussian_kl(mu_z, sigmasq_z)
     iwelbo_loss /= n_samples
-    
+
     # MSE loss
     mu_z, sigmasq_z = encode(encoder_params, images)
-    samples = gaussian_sample(predict_rng, mu_z, sigmasq_z) 
+    samples = gaussian_sample(predict_rng, mu_z, sigmasq_z)
     output = predict(predictor_params, samples, rng=predict_rng)
     error = labels - output
 
@@ -106,7 +108,7 @@ def iwelbo(rng, params, images, n_samples=32):
     return iwelbo_loss / n_samples
 
 
-def image_sample(rng, params, nrow, ncol):    
+def image_sample(rng, params, nrow, ncol):
     _, decoder_params, _ = params
     code_rng, image_rng = random.split(rng)
     # samples from the standard normal in latent space with shape (nrow * ncol, 10)
@@ -114,14 +116,14 @@ def image_sample(rng, params, nrow, ncol):
     logits = decode(decoder_params, latent_sample)
     sampled_images = random.bernoulli(image_rng, jnp.logaddexp(0., logits))
     sampled_images = jnp.logaddexp(0., logits)
-    
+
     return image_grid(nrow, ncol, sampled_images, IMAGE_SHAPE)
 
 
 def image_grid(nrow, ncol, image_vectors, image_shape):
-  images = iter(image_vectors.reshape((-1,) + image_shape))
-  return jnp.vstack([jnp.hstack([next(images).T for _ in range(ncol)][::-1])
-                    for _ in range(nrow)]).T
+    images = iter(image_vectors.reshape((-1,) + image_shape))
+    return jnp.vstack([jnp.hstack([next(images).T for _ in range(ncol)][::-1])
+                      for _ in range(nrow)]).T
 
 
 # define the VAE - one of FanOuts is softplus due to non-negative variance
@@ -132,7 +134,7 @@ def init_vanilla_vae():
         Dense(256), Relu,
         FanOut(2),
         stax.parallel(Dense(LATENT_SIZE), stax.serial(Dense(LATENT_SIZE), Softplus)),
-    )   
+    )
 
     decoder_init, decode = stax.serial(
         Dense(256), Relu,
@@ -143,26 +145,27 @@ def init_vanilla_vae():
     return encoder_init, encode, decoder_init, decode
 
 
-
 def mnist_regressor():
-    predictor_init, predict = stax.serial(Dense(128), Relu, 
-                                          Dense(128), Relu, 
+    predictor_init, predict = stax.serial(Dense(128), Relu,
+                                          Dense(128), Relu,
                                           Dense(1)
                                           )
     return predictor_init, predict
+
 
 def predict_image(rng, params, images):
     encoder_params, _, predictor_params = params
     mu_z, sigmasq_z = encode(encoder_params, images)
     samples = gaussian_sample(rng, mu_z, sigmasq_z)
     output = predict(predictor_params, samples, rng=rng, mode='test')
-    
+
     return output
 
 
 # TODO - define the quantity_of_interest that we want to optimize
 def quantity_of_interest():
     return 0
+
 
 if __name__ == '__main__':
     # if wanting to use a fully connected VAE or Convolutional (not yet implemented)
@@ -185,16 +188,15 @@ if __name__ == '__main__':
     test_rng = random.PRNGKey(1)  # fixed prng key for evaluation
     train_images, train_labels = load_mnist(train=True, reshape=reshape, fashion=FASHION)
     test_images, test_labels = load_mnist(train=False, fashion=FASHION)
-    train_images = train_images /255
-    test_images = test_images /255
+    train_images = train_images / 255
+    test_images = test_images / 255
     train_labels = train_labels / 9
     test_labels = test_labels / 9
     num_complete_batches, leftover = divmod(train_images.shape[0], batch_size)
     num_batches = num_complete_batches + bool(leftover)
-    
+
     imfile = os.path.join(os.path.join(os.getcwd(), "tmp/"), "mnist_vae_{:03d}.png")
     encoder_init_rng, decoder_init_rng, predictor_init_rng = random.split(random.PRNGKey(2), 3)
-    
 
     if vae_type == 'vanilla':
         define_vae = init_vanilla_vae
@@ -216,9 +218,8 @@ if __name__ == '__main__':
     test_images = jax.device_put(test_images[0:TEST_SIZE])
     test_labels = jax.device_put(test_labels[0:TEST_SIZE].reshape(-1, 1))
 
-
     def split_and_binarize_batch(rng, i, images, labels, binarize):
-        i  = i % num_batches
+        i = i % num_batches
         batch = lax.dynamic_slice_in_dim(images, i * batch_size, batch_size)
         batch_labels = lax.dynamic_slice_in_dim(labels, i * batch_size, batch_size)
 
@@ -228,19 +229,19 @@ if __name__ == '__main__':
     def run_epoch(rng, opt_state, images, labels, beta=1, binarize=False):
         def body_fun(i, opt_state):
             elbo_rng, data_rng = random.split(random.fold_in(rng, i))
-            batch, batch_labels = split_and_binarize_batch(data_rng, i, images, labels, binarize=binarize)
-            loss = lambda params: elbo_and_pred_loss(\
+            batch, batch_labels = split_and_binarize_batch(
+                data_rng, i, images, labels, binarize=binarize)
+            def loss(params): return elbo_and_pred_loss(
                 elbo_rng, params, batch, batch_labels, beta, pred_weight, n_samples=n_samples) / batch_size
             grads = grad(loss)(get_params(opt_state))
             return opt_update(i, grads, opt_state)
         return lax.fori_loop(0, num_batches, body_fun, opt_state)
 
-
     @jit
     def evaluate(opt_state, images, labels, binarize=False):
         params = get_params(opt_state)
         elbo_rng, data_rng, image_rng = random.split(test_rng, 3)
-        
+
         # Relic from when the MNIST images where binarized
         binarized_test = test_images
 
@@ -250,38 +251,40 @@ if __name__ == '__main__':
         latent_samples = sample_latent_space(elbo_rng, params, images)
         return test_elbo, test_mse, sampled_images, latent_samples
 
-
     opt_state = opt_init(init_params)
     beta_schedule = np.linspace(beta_init, beta_final, num_epochs)
     for epoch in range(num_epochs):
         tic = time.time()
-        opt_state = run_epoch(random.PRNGKey(epoch), opt_state, train_images, train_labels, beta=beta_schedule[epoch], binarize=binarize)
-        test_elbo, test_mse, sampled_images, latent_samples = evaluate(opt_state, test_images, test_labels, binarize=binarize)
+        opt_state = run_epoch(random.PRNGKey(epoch), opt_state, train_images,
+                              train_labels, beta=beta_schedule[epoch], binarize=binarize)
+        test_elbo, test_mse, sampled_images, latent_samples = evaluate(
+            opt_state, test_images, test_labels, binarize=binarize)
 
-        print("Ep. {: 3d} ---- ELBO: {} ---- MSE: {} ---- Time: {:.3f} sec".format(epoch, test_elbo, test_mse, time.time() - tic))
+        print("Ep. {: 3d} ---- ELBO: {} ---- MSE: {} ---- Time: {:.3f} sec".format(epoch,
+              test_elbo, test_mse, time.time() - tic))
         plt.imsave(imfile.format(epoch), sampled_images, cmap=plt.cm.gray)
-    plt.scatter(latent_samples[:, 0], latent_samples[:, 1], c=test_labels,\
-        cmap='viridis', s=4)
+    plt.scatter(latent_samples[:, 0], latent_samples[:, 1], c=test_labels,
+                cmap='viridis', s=4)
     cb = plt.colorbar()
     cb.set_ticklabels(list(range(10)))
-    
+
     # plot images and their prediction
     params = get_params(opt_state)
     elbo_rng, data_rng, image_rng = random.split(test_rng, 3)
-    binarized_test = random.bernoulli(data_rng, test_images) 
+    binarized_test = random.bernoulli(data_rng, test_images)
     sampled_images = np.random.choice(TEST_SIZE, 20, replace=False)
-    
+
     trained_params = optimizers.unpack_optimizer_state(opt_state)
     if FASHION:
         with open(f'models/trained_parameters_{LATENT_SIZE}_fashion.pkl', 'wb') as f:
             print(f'Saving model - trained_parameters_{LATENT_SIZE}_fashion.pkl...')
             pickle.dump(trained_params, f)
-    
+
     else:
         with open(f'models/trained_parameters_{LATENT_SIZE}.pkl', 'wb') as f:
             print(f'Saving model - trained_parameters_{LATENT_SIZE}.pkl...')
-            
+
             pickle.dump(trained_params, f)
     plt.show()
-    
+
 # TODO CHECK IN ENCODE WHAT SHAPE STUFF IS!
